6edfd294784ef9ad563b4e000783d90d7aed1f81
Ian <ianquah@hotmail.com>
2017-10-11 10:55:31 -0400

    ??

---
+++ auto/notes.el
@@ -0,0 +1,32 @@
+(TeX-add-style-hook
+ "notes"
+ (lambda ()
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "fancyhdr"
+    "amsfonts"
+    "bm"
+    "lastpage"
+    "extramarks"
+    "graphicx"
+    "lipsum"
+    "amsmath")
+   (TeX-add-symbols
+    '("problemAnswer" 1)
+    '("exitProblemHeader" 1)
+    '("enterProblemHeader" 1)
+    "homeworkProblemName"
+    "homeworkSectionName"
+    "hmwkClass"
+    "hmwkTitle"
+    "hmwkClassTime"
+    "hmwkAuthorName")
+   (LaTeX-add-environments
+    '("homeworkProblem" LaTeX-env-args ["argument"] 0)
+    '("homeworkSection" 1))
+   (LaTeX-add-counters
+    "homeworkProblemCounter"))
+ :latex)
+
