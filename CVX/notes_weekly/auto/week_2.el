e4823a6ee4da585d16260ceae70885818ceaeedf
Ian <ianquah@hotmail.com>
2017-10-30 18:44:49 -0400

    convex optimization over functions

---
+++ CVX/notes_weekly/auto/week_2.el
@@ -0,0 +1,34 @@
+(TeX-add-style-hook
+ "week_2"
+ (lambda ()
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "pdfpages"
+    "amssymb"
+    "fancyhdr"
+    "amsfonts"
+    "bm"
+    "lastpage"
+    "extramarks"
+    "graphicx"
+    "lipsum"
+    "amsmath")
+   (TeX-add-symbols
+    '("problemAnswer" 1)
+    '("exitProblemHeader" 1)
+    '("enterProblemHeader" 1)
+    "homeworkProblemName"
+    "homeworkSectionName"
+    "hmwkClass"
+    "hmwkTitle"
+    "hmwkClassTime"
+    "hmwkAuthorName")
+   (LaTeX-add-environments
+    '("homeworkProblem" LaTeX-env-args ["argument"] 0)
+    '("homeworkSection" 1))
+   (LaTeX-add-counters
+    "homeworkProblemCounter"))
+ :latex)
+
